d821b748dfc4edf9d67012eb23a4cfbfb2720c25
Patrick <dev@stoicsource.com>
2021-05-06 22:52:49 +0200

    get rid of vue material

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="app">
-    <md-toolbar>
-      <md-field>
-        <label for="translations">Translations</label>
-        <md-select v-model="selectedTranslations" name="translations" id="translations" multiple>
-          <md-option v-for="translation in translationMeta" :key="translation.key" :value="translation.key">{{ translation.author }} ({{ translation.year }})</md-option>
-        </md-select>
-      </md-field>
-    </md-toolbar>
-    <md-content>
-      <div v-if="loading">loading...</div>
-      <div v-if="translations">
-        <md-table>
-          <md-table-row>
-            <md-table-head></md-table-head>
-            <md-table-head v-for="translationInfo in selectedTranslationMeta" :key="translationInfo.key">{{ translationInfo.author }}</md-table-head>
-          </md-table-row>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-lg-2">
+        Translations to show
+        <v-select multiple :options="translationMeta" v-model="selectedTranslations" :reduce="translation => translation.key"></v-select>
 
-          <md-table-row v-for="section in translations" :key="section.SectionNumber">
-            <md-table-cell>{{ section.SectionNumber }}</md-table-cell>
-            <md-table-cell v-for="translationInfo in selectedTranslationMeta" :key="translationInfo.key">{{ section[translationInfo.key] }}</md-table-cell>
-          </md-table-row>
+        Sections
+        <div class="accordion" role="tablist">
+          <b-card no-body class="mb-1" v-for="book in sections" :key="book.key">
+            <b-card-header header-tag="header" class="p-1" role="tab" v-b-toggle="'collapseBook' + book.key">
+              Book {{ book.key }}
+            </b-card-header>
+            <b-collapse :id="'collapseBook' + book.key" accordion="my-accordion" role="tabpanel">
+              <b-card-body>
+                <b-card-text>
+                  <div v-for="chapter in book.sections" :key="book.key + '-' + chapter">{{ chapter }}</div>
+                </b-card-text>
+              </b-card-body>
+            </b-collapse>
+          </b-card>
+        </div>
 
-        </md-table>
       </div>
-    </md-content>
+      <div class="col-12 col-lg-10">
+        <div v-if="loading">loading...</div>
+        <div v-if="translations">
+          <table class="table">
+            <tr>
+              <th></th>
+              <th v-for="translationInfo in selectedTranslationMeta" :key="translationInfo.key">{{ translationInfo.author }}</th>
+            </tr>
+
+            <tr v-for="section in translations" :key="section.SectionNumber">
+              <td>{{ section.SectionNumber }}</td>
+              <td v-for="translationInfo in selectedTranslationMeta" :key="translationInfo.key">{{ section[translationInfo.key] }}</td>
+            </tr>
+
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,6 +54,7 @@ export default {
     axios.get(window.stoicsource.settings.apiUrl)
         .then(function (response) {
           this.translations = response.data;
+          this.extractSections();
         }.bind(this))
         .catch(function (error) {
           // handle error
@@ -60,6 +76,25 @@ export default {
       if (localStorage.selectedTranslations) {
         this.selectedTranslations = JSON.parse(localStorage.selectedTranslations);
       }
+    },
+    extractSections () {
+      this.sections = [];
+      this.translations.forEach((translation) => {
+        let numberParts = translation.SectionNumber.split('.');
+        let book = numberParts[0];
+        let chapter = numberParts[1] ;
+
+        let bookSection = this.sections.find((section) => section.key === book);
+        if (!bookSection) {
+          bookSection = {
+            key: book,
+            sections: []
+          };
+          this.sections.push(bookSection);
+        }
+
+        bookSection.sections.push(chapter);
+      });
     }
   },
   computed: {
@@ -68,78 +103,93 @@ export default {
     }
   },
   watch: {
-    selectedTranslations() {
+    selectedTranslations () {
       this.writeLocalStorage();
     }
   },
   data: () => ({
+    test: [],
     loading: false,
+    sections: [],
     translations: null,
     translationMeta: [
       {
         key: 'Casaubon',
         author: 'Meric Casaubon',
-        year: 1634
+        year: 1634,
+        label: 'Casaubon, Meric (1634)'
       },
       {
         key: 'Chrystal',
         author: 'George William Chrystal',
-        year: 1902
+        year: 1902,
+        label: 'Chrystal, George William (1902)'
       },
       {
         key: 'Collier',
         author: 'Jeremy Collier',
-        year: 1702
+        year: 1702,
+        label: 'Collier, Jeremy (1702)'
       },
       {
         key: 'Farquharson',
         author: 'A. S. L. Farquharson',
-        year: 1944
+        year: 1944,
+        label: 'Farquharson, A. S. L. (1944)'
       },
       {
         key: 'Graves',
         author: 'R. Graves',
-        year: 1792
+        year: 1792,
+        label: 'Graves, R. (1792)'
       },
       {
         key: 'Haines',
         author: 'C. R. Haines',
-        year: 1916
+        year: 1916,
+        label: 'Haines, C. R. (1916)'
       },
       {
         key: 'Jackson',
         author: 'J. Jackson',
-        year: 1906
+        year: 1906,
+        label: 'Jackson, J. (1906)'
       },
       {
         key: 'Long',
         author: 'George Long',
-        year: 1862
+        year: 1862,
+        label: 'Long, George (1862)'
       },
       {
         key: 'Maccormac',
         author: 'H. McCormac',
-        year: 1844
+        year: 1844,
+        label: 'McCormac, H. (1844)'
       },
       {
         key: 'Hutcheson',
         author: 'James Moor and Francis Hutcheson',
-        year: 1742
+        year: 1742,
+        label: 'Moor, James and Hutcheson, Francis (1742)'
       },
       {
         key: 'RendallV1',
         author: 'G. H. Rendall',
-        year: 1898
+        year: 1898,
+        label: 'Rendall, G. H. (1898)'
       },
       {
         key: 'RendallV2',
         author: 'G. H. Rendall',
-        year: 1901
+        year: 1901,
+        label: 'Rendall, G. H. (1901)'
       },
       {
         key: 'Thomson',
         author: 'James Thomson',
-        year: 1747
+        year: 1747,
+        label: 'Thomson, James (1747)'
       }
     ],
     selectedTranslations: ['Casaubon', 'Farquharson', 'RendallV2']
@@ -161,11 +211,6 @@ td, th {
   text-align: left;
   vertical-align: top;
   padding-bottom: 1em;
-}
-
-.md-menu-content.md-select-menu {
-  max-width: 480px;
-  max-height: 65vh;
 }
 
 </style>
