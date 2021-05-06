6cd5b4c4d3e4b85ce19d3730e94ea6eb2d127fcb
Patrick <patrick@vanberlin.com>
2021-05-06 22:52:49 +0200

    get rid of vue material

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-// import { MdSelect } from 'vue-material/dist/components'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import { BootstrapVue } from 'bootstrap-vue'
+import vSelect from "vue-select";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import "vue-select/dist/vue-select.css";
 
 Vue.use(VueRouter)
-Vue.use(VueMaterial)
+
+Vue.use(BootstrapVue)
+Vue.component("v-select", vSelect);
 
 Vue.config.productionTip = false
 
