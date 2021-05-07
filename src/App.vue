4773a762d1f0fedb7ea2cd3a0350d70ce04ee46a
Patrick <dev@stoicsource.com>
2021-05-07 16:47:50 +0200

    filter sections

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12 col-lg-2">
-        Translations to show
+      <div class="col-12 col-lg-3">
+        <div class="mt-2">Translations</div>
         <v-select multiple :options="translationMeta" v-model="selectedTranslations" :reduce="translation => translation.key"></v-select>
 
-        Sections
-        <div class="accordion" role="tablist">
-          <b-card no-body class="mb-1" v-for="book in sections" :key="book.key">
+        <div class="mt-3">Sections</div>
+        <v-select multiple :options="sectionsFlat" v-model="selectedSections"></v-select>
+
+        <div class="accordion mt-2" role="tablist">
+          <b-card no-body class="mb-1" v-for="book in sectionsTree" :key="book.key">
             <b-card-header header-tag="header" class="p-1" role="tab" v-b-toggle="'collapseBook' + book.key">
               Book {{ book.key }}
             </b-card-header>
             <b-collapse :id="'collapseBook' + book.key" accordion="my-accordion" role="tabpanel">
               <b-card-body>
                 <b-card-text>
-                  <div v-for="chapter in book.sections" :key="book.key + '-' + chapter">{{ chapter }}</div>
+                  <div v-for="chapter in book.sections" :key="book.key + '-' + chapter">
+                    {{ book.key }}.{{ chapter }}
+                  </div>
                 </b-card-text>
               </b-card-body>
             </b-collapse>
@@ -22,7 +26,7 @@
         </div>
 
       </div>
-      <div class="col-12 col-lg-10">
+      <div class="col-12 col-lg-9">
         <div v-if="loading">loading...</div>
         <div v-if="translations">
           <table class="table">
@@ -31,9 +35,9 @@
               <th v-for="translationInfo in selectedTranslationMeta" :key="translationInfo.key">{{ translationInfo.author }}</th>
             </tr>
 
-            <tr v-for="section in translations" :key="section.SectionNumber">
-              <td>{{ section.SectionNumber }}</td>
-              <td v-for="translationInfo in selectedTranslationMeta" :key="translationInfo.key">{{ section[translationInfo.key] }}</td>
+            <tr v-for="section in selectedSections" :key="section">
+              <td>{{ section }}</td>
+              <td v-for="translationInfo in selectedTranslationMeta" :key="translationInfo.key">{{ findSectionData(section)[translationInfo.key] }}</td>
             </tr>
 
           </table>
@@ -71,30 +75,41 @@ export default {
     },
     writeLocalStorage () {
       localStorage.selectedTranslations = JSON.stringify(this.selectedTranslations);
+      localStorage.selectedSections = JSON.stringify(this.selectedSections);
     },
     readLocalStorage () {
       if (localStorage.selectedTranslations) {
         this.selectedTranslations = JSON.parse(localStorage.selectedTranslations);
       }
+      if (localStorage.selectedSections) {
+        this.selectedSections = JSON.parse(localStorage.selectedSections);
+      }
     },
     extractSections () {
-      this.sections = [];
+      this.sectionsTree = [];
+      this.sectionsFlat = [];
       this.translations.forEach((translation) => {
+        this.sectionsFlat.push(translation.SectionNumber);
+
         let numberParts = translation.SectionNumber.split('.');
         let book = numberParts[0];
         let chapter = numberParts[1] ;
 
-        let bookSection = this.sections.find((section) => section.key === book);
+        let bookSection = this.sectionsTree.find((section) => section.key === book);
         if (!bookSection) {
           bookSection = {
             key: book,
             sections: []
           };
-          this.sections.push(bookSection);
+          this.sectionsTree.push(bookSection);
         }
 
         bookSection.sections.push(chapter);
       });
+    },
+    findSectionData (sectionNumber) {
+      let matches = this.translations.filter((section) => section.SectionNumber === sectionNumber);
+      return matches.length > 0 ? matches[0] : [];
     }
   },
   computed: {
@@ -105,12 +120,16 @@ export default {
   watch: {
     selectedTranslations () {
       this.writeLocalStorage();
+    },
+    selectedSections () {
+      this.writeLocalStorage();
     }
   },
   data: () => ({
-    test: [],
+    selectedSections: [],
     loading: false,
-    sections: [],
+    sectionsTree: [],
+    sectionsFlat: [],
     translations: null,
     translationMeta: [
       {
