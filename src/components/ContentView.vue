73336a26686bcfe1b3ad1ed2795f7b5158995c89
Patrick <patrick@vanberlin.com>
2021-07-01 20:46:50 +0200

    content view

--- src/components/ContentView.vue
+++ src/components/ContentView.vue
@@ -1,9 +1,23 @@
 <template>
   <div v-if="selectedWorkId">
-    {{ selectedWork.name }}
-    <div v-for="edition in selectedWork.editions" :key="edition.id">
-      <div v-if="edition.checked">{{ edition.authorsFormatted }} ({{ edition.year }})</div>
-    </div>
+
+    <table class="table">
+      <tr>
+        <th class="d-none d-lg-table-cell"></th>
+        <th v-for="edition in selectedEditions" :key="edition.id">{{ edition.authorsFormatted }} ({{ edition.year }})</th>
+      </tr>
+
+      <tr v-for="tocEntry in selectedTocEntries" :key="tocEntry.id">
+        <td class="d-none d-lg-table-cell">{{ tocEntry.label }}</td>
+        <td v-for="(edition, index) in selectedEditions" :key="edition.id" class="translation-section">
+          <div class="translation-content">
+            <span v-if="index === 0" class="d-lg-none"><strong>{{ tocEntry.label }}</strong></span>
+            TOC {{ tocEntry.id }} for Edition {{ edition.id }}
+            <span class="quote-translation" @click="quoteTranslation(section, translationInfo.key)">quote</span>
+          </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -22,7 +36,15 @@ export default {
     ]),
 
     selectedWork () {
-      return Work.query().where('id', this.selectedWorkId).with(['editions', 'editions.authors']).first();
+      return Work.query().where('id', this.selectedWorkId).with(['editions.authors', 'tocEntries']).first();
+    },
+
+    selectedEditions () {
+      return this.selectedWork.editions.filter((edition) => edition.selected );
+    },
+
+    selectedTocEntries () {
+      return this.selectedWork.tocEntries.filter((tocEntry) => tocEntry.selected );
     }
   },
   methods: {}
