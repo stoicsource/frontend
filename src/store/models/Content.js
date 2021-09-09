fae219a2aa9082ca9cd140a51b9ca272a00f8f18
Patrick <dev@stoicsource.com>
2021-09-09 22:17:05 +0200

    content: add optional title

--- src/store/models/Content.js
+++ src/store/models/Content.js
@@ -12,6 +12,7 @@ export default class Content extends Model {
       edition_id: this.attr(null),
       content: this.attr(''),
       notes: this.attr(''),
+      title: this.attr(''),
 
       tocEntry: this.belongsTo(TocEntry, 'toc_entry_id'),
       edition: this.belongsTo(Edition, 'edition_id')
