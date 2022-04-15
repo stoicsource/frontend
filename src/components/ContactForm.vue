66710bd414140c49923bb7287c3dbcdd4f86a518
Patrick <patrick@vanberlin.com>
2022-04-15 10:01:55 +0200

    change contact form to just email address

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,39 +1,12 @@
 <template>
   <b-modal id="contact-modal" title="Contact">
-    <b-form>
-      <ValidationProvider name="Name" rules="required" v-slot="{ errors }">
-        <b-form-group>
-          <b-form-input v-model="contactName" placeholder="Name" name="email" required autofocus></b-form-input>
-          <span>{{ errors[0] }}</span>
-        </b-form-group>
-      </ValidationProvider>
-
-      <ValidationProvider name="E-mail" rules="required|email" v-slot="{ errors }">
-        <b-form-group>
-          <b-form-input v-model="contactEmail" placeholder="Email" required type="email"></b-form-input>
-          <span>{{ errors[0] }}</span>
-        </b-form-group>
-      </ValidationProvider>
-
-      <ValidationProvider name="Message" rules="required" v-slot="{ errors }">
-        <b-form-group>
-          <b-form-textarea
-              id="textarea"
-              v-model="contactMessage"
-              placeholder="Message..."
-              rows="6"
-              required
-          ></b-form-textarea>
-          <span>{{ errors[0] }}</span>
-        </b-form-group>
-      </ValidationProvider>
-    </b-form>
+    <div>
+      Send us an email to <a href="mailto:feedback@stoicsource.com">feedback@stoicsource.com</a><br>
+      We'd love to hear from you!
+    </div>
     <template #modal-footer="{ cancel }">
-      <b-button size="sm" variant="primary" @click="send()">
-        Send
-      </b-button>
       <b-button size="sm" @click="cancel()">
-        Cancel
+        Close
       </b-button>
     </template>
   </b-modal>
@@ -41,23 +14,8 @@
 
 <script>
 import Author from "@/store/models/Author";
-import {ValidationProvider, extend} from 'vee-validate';
-import {required, email} from 'vee-validate/dist/rules';
-import Swal from 'sweetalert2'
-
-const axios = require('axios');
-
-extend('email', email);
-
-extend('required', {
-  ...required,
-  message: 'This field is required'
-});
 
 export default {
-  components: {
-    ValidationProvider
-  },
   data () {
     return {
       contactName: '',
@@ -71,31 +29,7 @@ export default {
     }
   },
   methods: {
-    send () {
-      if (this.contactName && this.contactEmail && this.contactMessage) {
-        const formData = new FormData();
-        formData.append('name', this.contactName);
-        formData.append('email', this.contactEmail);
-        formData.append('message', this.contactMessage);
 
-        // axios.post(process.env.VUE_APP_API_URL + '/contact', formData, { withCredentials: true })
-        axios.post(process.env.VUE_APP_API_URL + '/contact', formData)
-            .then(function () {
-              this.contactName = '';
-              this.contactEmail = '';
-              this.contactMessage = '';
-
-              Swal.fire({
-                title: 'Message submitted',
-                text: 'Thank you for your feedback!',
-                showCancelButton: false,
-                confirmButtonText: 'Ok'
-              });
-            }.bind(this));
-
-        this.$bvModal.hide('contact-modal');
-      }
-    }
   }
 }
 </script>
