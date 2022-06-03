339b00839d3d59b9f8e9eaa8dc96bfd0d07ea49d
Patrick <dev@stoicsource.com>
2022-06-03 21:21:58 +0200

    add repo info to contact dialog

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -7,8 +7,13 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          Send us an email to <a href="mailto:feedback@stoicsource.com">feedback@stoicsource.com</a><br>
-          We'd love to hear from you!
+          <p>
+            Send us an email to <a href="mailto:feedback@stoicsource.com">feedback@stoicsource.com</a><br>
+            We'd love to hear from you!
+          </p>
+          <p>
+            <small>Developers, you can find us at <a href="https://github.com/stoicsource" target="_blank">https://github.com/stoicsource</a></small>
+          </p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -34,8 +39,6 @@ export default {
       return Author.query().has('works').withAllRecursive().orderBy('shortestName').all();
     }
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
