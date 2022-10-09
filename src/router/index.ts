36d75db308a67f21772cbdf51a645ea234c72206
Patrick <dev@stoicsource.com>
2022-10-09 17:21:18 +0200

    route level code-splitting

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import AuthorSelect from "../views/AuthorSelect.vue";
-import WorkSelect from "../views/WorkSelect.vue";
-import ChapterView from "../views/ChapterView.vue";
-import EditionInfoView from "../views/EditionInfoView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,42 +24,45 @@ const router = createRouter({
     },
 
     // Starting screen
-    { path: "/", component: AuthorSelect, name: "authorSelect" },
+    {
+      path: "/",
+      component: () => import("../views/AuthorSelect.vue"),
+      name: "authorSelect",
+    },
 
     // Information about a specific edition
     {
       path: "/edition/:editionId/info",
-      component: EditionInfoView,
       props: true,
       name: "editionInfo",
+      component: () => import("../views/EditionInfoView.vue"),
     },
 
     // Work Select
-    { path: "/:authorSlug/works", component: WorkSelect, props: true },
+    {
+      path: "/:authorSlug/works",
+      component: () => import("../views/WorkSelect.vue"),
+      props: true,
+    },
 
     // Main content view
-    { path: "/:author/:workSlug", component: ChapterView, props: true },
+    {
+      path: "/:author/:workSlug",
+      component: () => import("../views/ChapterView.vue"),
+      props: true,
+    },
     {
       path: "/:author/:workSlug/:tocSlug",
-      component: ChapterView,
+      component: () => import("../views/ChapterView.vue"),
       props: true,
       name: "contentByToc",
     },
     {
       path: "/:author/:workSlug/:tocSlug/:translatorSlug",
-      component: ChapterView,
+      component: () => import("../views/ChapterView.vue"),
       props: true,
       name: "contentByTocAndTranslator",
     },
-
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
   ],
 });
 
